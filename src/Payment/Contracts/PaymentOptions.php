5633618f2d57fc42a0a3a19e5cd281c8452f35b4
Jakub Theimer <theimer@madne.st>
2020-06-22 04:38:32 +0200

    edit contract

--- src/Payment/Contracts/PaymentOptions.php
+++ src/Payment/Contracts/PaymentOptions.php
@@ -3,8 +3,9 @@
 namespace Madnest\Madstore\Payment\Contracts;
 
 use Madnest\Madstore\Payment\Contracts\Purchasable;
+use Madnest\Madstore\Payment\PaymentResponse;
 
 interface PaymentOption
 {
-    public function createPayment(Purchasable $purchasable, array $params = [], array $options = []);
+    public function createPayment(Purchasable $purchasable, array $params = [], array $options = []): PaymentResponse;
 }
