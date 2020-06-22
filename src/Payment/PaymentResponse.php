8ee3342fa09d71aeb9a0be2d3055fb1121ac8b87
Jakub Theimer <theimer@madne.st>
2020-06-22 04:38:40 +0200

    add payment response

---
+++ src/Payment/PaymentResponse.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Madnest\Madstore\Payment;
+
+use Madnest\Madstore\Core\Response;
+
+class PaymentResponse extends Response
+{
+    public int $statusCode;
+
+    public string $status;
+
+    public string $orderNumber;
+
+    public int $amount;
+
+    public string $currency;
+
+    public array $payer;
+
+    public bool $redirect;
+
+    public string $redirectUrl;
+
+    public function __construct(
+        int $statusCode,
+        string $status,
+        string $orderNumber,
+        int $amount,
+        string $currency,
+        array $payer,
+        string $redirectUrl,
+        bool $redirect
+    ) {
+        $this->statusCode = $statusCode;
+        $this->status = $status;
+        $this->orderNumber = $orderNumber;
+        $this->amount = $amount;
+        $this->currency = $currency;
+        $this->payer = $payer;
+        $this->redirectUrl = $redirectUrl;
+        $this->redirect = $redirect;
+    }
+}
