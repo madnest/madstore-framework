1a94cdf17b4aaf9cc9309cae636913311bafbdf9
Jakub Theimer <theimer@madne.st>
2020-06-22 03:58:08 +0200

    add payable contract

---
+++ src/Payment/Contracts/Payable.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Madnest\Madstore\Payment\Contracts;
+
+interface Payable
+{
+    /**
+     * Determines wether Payment option was already set
+     *
+     * @return boolean
+     */
+    public function hasPaymentOption(): bool;
+
+    /**
+     * Get Payment option
+     *
+     * @return PaymentOption
+     */
+    public function paymentOption(): PaymentOption;
+}
