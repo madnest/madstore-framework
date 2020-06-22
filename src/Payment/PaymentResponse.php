536e56808b0407698ecdf5389160092253e5f764
Jakub Theimer <theimer@madne.st>
2020-06-22 05:31:16 +0200

    refactor payment response

--- src/Payment/PaymentResponse.php
+++ src/Payment/PaymentResponse.php
@@ -26,23 +26,58 @@ class PaymentResponse extends Response
 
     public function __construct(
         int $statusCode,
-        string $status,
-        string $orderNumber,
-        int $amount,
-        string $currency,
-        array $payer,
-        string $redirectUrl,
-        bool $redirect,
-        array $errors
+        string $status
     ) {
         $this->statusCode = $statusCode;
         $this->status = $status;
+    }
+
+    public function setOrderNumber(string $orderNumber): self
+    {
         $this->orderNumber = $orderNumber;
+
+        return $this;
+    }
+
+    public function setAmount(string $amount): self
+    {
         $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function setCurrency(string $currency)
+    {
         $this->currency = $currency;
+
+        return $this;
+    }
+
+    public function setPayer(array $payer)
+    {
         $this->payer = $payer;
+
+        return $this;
+    }
+
+    public function setRedirectUrl(string $redirectUrl)
+    {
         $this->redirectUrl = $redirectUrl;
+
+        return $this;
+    }
+
+    public function setRedirect(bool $redirect)
+    {
         $this->redirect = $redirect;
+
+        return $this;
+    }
+
+    public function setErrors(array $errors)
+    {
         $this->errors = $errors;
+
+        return $this;
     }
 }
