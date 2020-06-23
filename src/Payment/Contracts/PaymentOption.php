80143b03a746e3914ae3945da7f245d0075c4cb4
Jakub Theimer <theimer@madne.st>
2020-06-23 21:36:02 +0200

    add method and comments

--- src/Payment/Contracts/PaymentOption.php
+++ src/Payment/Contracts/PaymentOption.php
@@ -7,5 +7,21 @@ use Madnest\Madstore\Payment\PaymentResponse;
 
 interface PaymentOption
 {
+    /**
+     * Creates new payment
+     *
+     * @param Purchasable $purchasable
+     * @param array $params
+     * @param array $options
+     * @return PaymentResponse
+     */
     public function createPayment(Purchasable $purchasable, array $params = [], array $options = []): PaymentResponse;
+
+    /**
+     * Get status of payment
+     *
+     * @param int $id
+     * @return PaymentResponse
+     */
+    public function getStatus(int $id): PaymentResponse;
 }
